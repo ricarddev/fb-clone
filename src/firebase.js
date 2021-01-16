8f73b61af130642ca793e26db65ea09807e6c29e
ricarddev <74429701+ricarddev@users.noreply.github.com>
2021-01-16 17:06:04 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,16 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBSgGvZKeD_DuXT071cHkpb48JHUrHlvQo",
-    authDomain: "facebook-clone-ffdac.firebaseapp.com",
-    projectId: "facebook-clone-ffdac",
-    storageBucket: "facebook-clone-ffdac.appspot.com",
-    messagingSenderId: "785220534497",
-    appId: "1:785220534497:web:e9bbff52c06b3c0c6db7a0",
-    measurementId: "G-CK42HQR7JT"
+
+//     here paste your config from firebase      
+    
+//     apiKey: "your api key",
+//     authDomain: "",
+//     projectId: "",
+//     storageBucket: "",
+//     messagingSenderId: "",
+//     appId: "",
+//     measurementId: ""
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
